67a9a9ba8aa82e193304dc8abd72a272e7470cf1
Beast-single-leaked <88524346+Beast-single-leaked@users.noreply.github.com>
2021-08-06 09:45:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//? playing the audio 
+
+
+let btn=document.querySelector("#btn");
+
+btn.addEventListener("click",()=>{
+    let audio=document.querySelector("#audio");
+         audio.play();
+         
+})
+
